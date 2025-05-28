d6f4edd6cba297d207a4da778afe3af88cf9da98
mahiraguliyeva <mahiraguliyava2@gmail.com>
2025-05-28 10:18:45 +0400

    Add files via upload

---
+++ project_code.py
@@ -0,0 +1,8 @@
+# project_code.py
+
+print("This is a placeholder for full project Python code including:")
+print("- graph_module")
+print("- stack_queue_module")
+print("- data_operations")
+print("- performance")
+print("- CLI main menu")
